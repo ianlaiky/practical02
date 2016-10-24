d8ea92e0189962e2a7b5eda1b771f1251da14269
Ian <152772a@mymail.nyp.edu.sg>
2016-10-24 17:01:31 +0800

    1

--- src/demo/BookCatalogServlet.java
+++ src/demo/BookCatalogServlet.java
@@ -13,8 +13,7 @@ import java.io.IOException;
 @WebServlet(name = "BookCatalogServlet",urlPatterns = "/bookcatalog")
 public class BookCatalogServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
+}
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
