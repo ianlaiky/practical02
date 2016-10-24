8b57a6488e291487d8bfe2283644d137c5bb6b9a
152772a <152772a@mymail.nyp.edu.sg>
2016-10-24 17:32:46 +0800

    1

--- src/demo/BookCatalogServlet.java
+++ src/demo/BookCatalogServlet.java
@@ -1,22 +1,64 @@
 package demo;
 //ttt
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by 152772a on 10/24/2016.
  */
-@WebServlet(name = "BookCatalogServlet",urlPatterns = "/bookcatalog")
+@WebServlet(name = "BookCatalogServlet", urlPatterns = "/bookcatalog")
 public class BookCatalogServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-}
+    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<html>" + "<head><title>Duke's Bookstore</title></head>" +
+                "<body  bgcolor=\"#ffffff\">" + "<center>" +
+                "<hr> <br> &nbsp;" + "<h1>" +
+                "<font size=\"+3\" color=\"#CC0066\">Duke's </font> <img src=\"" +
+                "./duke.books.gif\" alt=\"Duke holding books\"\">" +
+                "<font size=\"+3\" color=\"black\">Bookstore</font>" + "</h1>" +
+                "</center>" + "<br> &nbsp; <hr> <br> ");
 
+
+        BookDBAO db = new BookDBAO();
+        List<BookDetails> bd = db.getAllList();
+
+
+      try {
+
+          out.println("<html>");
+          out.println("<body>");
+          out.println("<table>");
+
+
+          for (int i = 0; i < bd.size(); i++) {
+
+              out.println("<tr>");
+              out.println("<td>" + bd.get(i).getTitle() + "</td>");
+              out.println("<td>" + bd.get(i).getDescription() + "</td>");
+              out.println("<td>" + bd.get(i).getYear() + "</td>");
+              out.println("</tr>");
+          }
+          out.println("</table>");
+          out.println("</body>");
+          out.println("</html>");
+
+          out.close();
+      }catch (Exception e){
+          e.printStackTrace();
+          throw new ServletException(e);
+
+        }
 
 
 
--- src/demo/BookDBAO.java
+++ src/demo/BookDBAO.java
@@ -1,4 +1,8 @@
 package demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by chitboon on 10/23/15.
  */
@@ -8,6 +12,56 @@ public class BookDBAO {
     public BookDetails getBookDetails(String bookId) {
         // return dummy book for testing now
         return createDummyBook(bookId);
+    }
+
+    public List<BookDetails> getAllList() {
+
+        ArrayList<BookDetails> ba = new ArrayList<BookDetails>();
+
+
+        BookDetails b1 = new BookDetails();
+        b1.setBookId("1");
+        b1.setDescription("hhherrw");
+        b1.setFirstName("Jeeava");
+        b1.setSurname("Exeqwpert");
+        b1.setInventory(5);
+        b1.setOnSale(false);
+        b1.setPrice(5.9f);
+        b1.setTitle("Web components for developers");
+        b1.setYear(2015);
+
+
+        BookDetails b2 = new BookDetails();
+        b2.setBookId("2");
+        b2.setDescription("hrrrrrrhherrw");
+        b2.setFirstName("Jeerwwava");
+        b2.setSurname("Exeqwpert");
+        b2.setInventory(5);
+        b2.setOnSale(false);
+        b2.setPrice(5.9f);
+        b2.setTitle("Web components for developers");
+        b2.setYear(2015);
+
+
+        BookDetails b3 = new BookDetails();
+        b3.setBookId("3");
+        b3.setDescription("hhrwerweherrw");
+        b3.setFirstName("Jrrrreeava");
+        b3.setSurname("Exeqwpert");
+        b3.setInventory(5);
+        b3.setOnSale(false);
+        b3.setPrice(4.9f);
+        b3.setTitle("Web components for developers");
+        b3.setYear(2015);
+
+
+        ba.add(b1);
+        ba.add(b2);
+        ba.add(b3);
+
+        return ba;
+
+
     }
 
     // convenient method to create a dummy book detail
